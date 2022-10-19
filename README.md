8fb17e5d8052bd8278b19b9ee5c8d447b4ff1db4
ahmedosama7450 <a.osama7450@gmail.com>
2022-10-19 14:51:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fwd-embedded-systems-traffic-light-control
---
+++ fwd-traffic-light-control/main.c
@@ -0,0 +1,18 @@
+/*
+ * fwd-traffic-light-control.c
+ *
+ * Created: 10/19/2022 11:37:38 AM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
