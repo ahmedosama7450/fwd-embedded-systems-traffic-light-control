3aec294dc5c8bf62f655df7ffbb4c914cdb18d82
ahmedosama7450 <a.osama7450@gmail.com>
2022-11-06 23:21:01 +0200

    build

--- fwd-traffic-light-control/Debug/MCAL/DIO/test.d
+++ fwd-traffic-light-control/Debug/MCAL/DIO/test.d
@@ -1,8 +1,16 @@
 MCAL/DIO/test.d MCAL/DIO/test.o: ../MCAL/DIO/test.c ../MCAL/DIO/dio.h \
- ../MCAL/DIO/../../Utils/helpers.h ../MCAL/DIO/../../Utils/registers.h
+ ../MCAL/DIO/../../Utils/helpers.h ../MCAL/DIO/../../Utils/registers.h \
+ ../MCAL/DIO/../Timer/timer.h ../MCAL/DIO/../Timer/../../Utils/helpers.h \
+ ../MCAL/DIO/../Timer/../../Utils/registers.h
 
 ../MCAL/DIO/dio.h:
 
 ../MCAL/DIO/../../Utils/helpers.h:
 
 ../MCAL/DIO/../../Utils/registers.h:
+
+../MCAL/DIO/../Timer/timer.h:
+
+../MCAL/DIO/../Timer/../../Utils/helpers.h:
+
+../MCAL/DIO/../Timer/../../Utils/registers.h:
--- fwd-traffic-light-control/Debug/MCAL/Interrupts/test.d
+++ fwd-traffic-light-control/Debug/MCAL/Interrupts/test.d
@@ -1,9 +1,27 @@
 MCAL/Interrupts/test.d MCAL/Interrupts/test.o: ../MCAL/Interrupts/test.c \
  ../MCAL/Interrupts/interrupts.h ../MCAL/Interrupts/../../Utils/helpers.h \
- ../MCAL/Interrupts/../../Utils/registers.h
+ ../MCAL/Interrupts/../../Utils/registers.h \
+ ../MCAL/Interrupts/../DIO/dio.h \
+ ../MCAL/Interrupts/../DIO/../../Utils/helpers.h \
+ ../MCAL/Interrupts/../DIO/../../Utils/registers.h \
+ ../MCAL/Interrupts/../Timer/timer.h \
+ ../MCAL/Interrupts/../Timer/../../Utils/helpers.h \
+ ../MCAL/Interrupts/../Timer/../../Utils/registers.h
 
 ../MCAL/Interrupts/interrupts.h:
 
 ../MCAL/Interrupts/../../Utils/helpers.h:
 
 ../MCAL/Interrupts/../../Utils/registers.h:
+
+../MCAL/Interrupts/../DIO/dio.h:
+
+../MCAL/Interrupts/../DIO/../../Utils/helpers.h:
+
+../MCAL/Interrupts/../DIO/../../Utils/registers.h:
+
+../MCAL/Interrupts/../Timer/timer.h:
+
+../MCAL/Interrupts/../Timer/../../Utils/helpers.h:
+
+../MCAL/Interrupts/../Timer/../../Utils/registers.h:
